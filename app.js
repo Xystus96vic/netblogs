a82fd4fae417d2cfcc3733de35ad055310c0949d
Xystus96vic <116673658+Xystus96vic@users.noreply.github.com>
2022-11-05 11:43:22 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.listen(3000);
+
+app.get('./', (req, res) => {
+  res.sendFile('./HTML/index.html', {root: __dirname});
+});
+
---
+++ main.js
@@ -0,0 +1,26 @@
+let thankyou = setTimeout(() => {alert('Xystus says thank you for visiting my website')}, 2000);
+  
+const click = document.getElementById('buton');
+click.addEventListener('click', () => {
+
+    var txt = document.getElementById('txt').value;
+     
+     if(!txt){
+       alert('please write your coment to submit')
+       return;
+       
+     }else{
+        const mess = document.createElement('div');
+        mess.classList.add('mess');
+        
+        const p = document.createElement('p');
+       
+        const text = document.createTextNode(txt);
+        p.appendChild(text);
+        mess.appendChild(p);
+    
+    document.getElementById('pwrapper').appendChild(mess);
+    }
+  });
+
+
